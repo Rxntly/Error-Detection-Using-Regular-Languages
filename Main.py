67d81e31a8f860eea64a18736f40420693b482bc
ryan <35403012+Rxntly@users.noreply.github.com>
2024-12-26 20:03:28 +0200

    Update Main.py
    theory of computations code

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
-# Created by Raed Massoud on 12/21/2024
-## This model uses **ODD PARITY**
+## This code checks for  **ODD PARITY**
 
-### Main Method
 def parityCheck(string):
 
-    message = string[:-1] # remove parity bit
-    parityBit = string[-1] # store parity bit
+    message = string[:-1]  # remove parity bit
+    parityBit = string[-1]  # store parity bit
 
     count = 0
 
@@ -24,33 +22,67 @@ def parityCheck(string):
     if parityBit == parity:
         return "Good Integrity"
     else:
-        return "Possible Corruption"
+        return "Bad Integrity"
     
-### End of Main
-    
+
 # Test Bench
 testBench = [
-    "1010100", # 3 1s parity bit should be 0 (Correct)
-    "0000000", # 0 1s parity bit should be 1 (False)
-    "11111011", # 6 1s parity bit should be 1 (Correct)
-    "1101001", # 3 1s parity bit should be 0 (False)
-    "011",     # 1 1  parity bit should be 0 (False)
-    "0000001", # 0 1s parity bit should be 1 (Correct)
-    "1111001", # 4 1s parity bit should be 1 (Correct)
-    "111111"   # 5 1s parity bit should be 0 (False)
-    "1000011", # 2 1s parity bit should be 1 (Correct)
-    "110",     # 2 1s parity bit should be 1 (False)
+    "1010100",  # (Correct)
+    "0000000",  # (False)
+    "11111011", # (Correct)
+    "1101001",  # (False)
+    "011",      # (False)
+    "0000001",  # (Correct)
+    "1111001",  # (Correct)
+    "111111",   # (False)
+    "1000011",  # (Correct)
+    "110",      # (False)
 ]
 
-# Run test bench
-for test in testBench:
+def showTestCases():
+    for test in testBench:
+        # Purely to make output a little more readable
+        prettyOutput = ""
+        printAfter = 20
+        for x in range(abs(printAfter - len(test))):
+            prettyOutput += "-" 
+        # Prints output
+        print(f"Input: {test} {prettyOutput}> {parityCheck(test)}")
 
-    # Purely to make out put readable / pretty
-    # Please increase "print after" for string longer than 19 bits
-    prettyOutput = ""
-    printAfter = 20
-    for x in range(abs(printAfter - len(test))):
-        prettyOutput += "-" 
 
-    # Print output
-    print(f"Input: {test} {prettyOutput}> {parityCheck(test)}")
+def main():
+    while True:
+        print("\nChoose an option:")
+        print("1. Enter your own input")
+        print("2. See pre-existing test cases")
+        print("3. Quit")
+
+        choice = input("Enter your choice (1, 2, or 3): ")
+
+        if choice == '1':
+            while True:
+                user_input = input("Enter a binary string (with parity bit): ")
+                # Check if the input is a valid binary string
+                if all(bit in '01' for bit in user_input):
+                    print(f"Input: {user_input} > {parityCheck(user_input)}")
+                    
+                    # Ask if they want to make another guess
+                    another_guess = input("Would you like to enter another binary string? (y/n): ").lower()
+                    if another_guess != 'y':
+                        break 
+                else:
+                    print("Invalid input! Please enter a binary string (only '0' and '1').")
+        
+        elif choice == '2':
+            showTestCases()
+        
+        elif choice == '3':
+            print("Exiting the program.")
+            break
+        
+        else:
+            print("Invalid choice! Please select 1, 2, or 3.")
+
+# Run code
+if __name__ == "__main__":
+    main()
